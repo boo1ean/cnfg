d30ea6a2326d37b3f865e38beea2735d862ef1c3
boo1ean <boo1ean0807@gmail.com>
2014-01-17 14:22:54 +0200

    Add test for two-level configuration

--- test/cnfg.js
+++ test/cnfg.js
@@ -3,13 +3,33 @@ var cnfg = require('../');
 describe('Hierarchical environment config extraction', function() {
 	it('Should extract single-level config very well', function() {
 		var path = __dirname + '/config-single-level';
-		var config = cnfg(path, 'prod');
 		var expected = {
 			'api': { key: '123' },
 			'app': { transport: 'http' },
 			'db': { name: 'johny' }
 		};
 
+		var config = cnfg(path);
+
 		JSON.stringify(config).should.be.equal(JSON.stringify(expected));
+	});
+
+	it('Should extract two-level configuration like a boss', function() {
+		var path = __dirname + '/config-two-levels';
+		var expected = JSON.stringify({
+			'api': { key: '123' },
+			'app': { transport: 'http' },
+			'db': { name: 'johny' }
+		});
+
+		JSON.stringify(cnfg(path)).should.be.equal(expected);
+
+		var expected_prod = JSON.stringify({
+			'api': { key: 'prod_123' },
+			'app': { transport: 'http' },
+			'db': { name: 'prod_johny' }
+		});
+
+		JSON.stringify(cnfg(path, 'prod')).should.be.equal(expected_prod);
 	});
 });
