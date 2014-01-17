edc16b7750e251c363e2f962a8d355a88eabd42f
boo1ean <boo1ean0807@gmail.com>
2014-01-17 16:32:53 +0200

    Replace should with chai, rewrite tests

--- test/cnfg.js
+++ test/cnfg.js
@@ -1,4 +1,5 @@
-var cnfg = require('../');
+var cnfg = require('../'),
+    expect = require('chai').expect;
 
 describe('Hierarchical environment config extraction', function() {
 	it('Should extract single-level config very well', function() {
@@ -11,55 +12,55 @@ describe('Hierarchical environment config extraction', function() {
 
 		var config = cnfg(path);
 
-		JSON.stringify(config).should.be.equal(JSON.stringify(expected));
+		expect(config).to.be.deep.equal(expected);
 	});
 
 	it('Should extract two-level configuration like a boss', function() {
 		var path = __dirname + '/config-two-levels';
-		var expected = JSON.stringify({
+		var expected = {
 			'api': { key: '123' },
 			'app': { transport: 'http' },
 			'db': { name: 'johny' }
-		});
+		};
 
-		JSON.stringify(cnfg(path)).should.be.equal(expected);
+		expect(cnfg(path)).to.be.deep.equal(expected);
 
-		var expected_prod = JSON.stringify({
+		var expected_prod = {
 			'api': { key: 'prod_123' },
 			'app': { transport: 'http' },
 			'db': { name: 'prod_johny', port: 5555 }
-		});
+		};
 
-		JSON.stringify(cnfg(path, 'prod')).should.be.equal(expected_prod);
+		expect(cnfg(path, 'prod')).to.be.deep.equal(expected_prod);
 
-		var expected_test = JSON.stringify({
+		var expected_test = {
 			'api': { key: '123', test_secret: 'wow such secret' },
 			'app': { transport: 'http' },
 			'db': { name: 'johny' }
-		});
+		};
 
-		JSON.stringify(cnfg(path, 'test')).should.be.equal(expected_test);
+		expect(cnfg(path, 'test')).to.be.deep.equal(expected_test);
 	});
 
 	it('Should extract configuration with index.js in base dir', function() {
-		var expected = JSON.stringify({
+		var expected = {
 			'api': { key: '123' },
 			'app': { transport: 'http' },
 			'db': { name: 'johny' }
-		});
+		};
 
-		JSON.stringify(require('./config-with-index')).should.be.equal(expected);
+		expect(require('./config-with-index')).to.be.deep.equal(expected);
 	});
 
 	it('Should extract two-level configuration with index', function() {
 		var path = './config-two-levels-with-index';
 
-		var expected_prod = JSON.stringify({
+		var expected_prod = {
 			'api': { key: 'prod_123' },
 			'app': { transport: 'http' },
 			'db': { name: 'prod_johny', port: 5555 }
-		});
+		};
 
-		JSON.stringify(require(path)).should.be.equal(expected_prod);
+		expect(require(path)).to.be.deep.equal(expected_prod);
 	});
 });
