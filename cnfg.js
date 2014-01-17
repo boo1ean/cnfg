7b411c4ab4be285de6cd1fdb20e53affdfefdf7a
boo1ean <boo1ean0807@gmail.com>
2014-01-17 13:46:00 +0200

    Fix multi-level config extraction

--- cnfg.js
+++ cnfg.js
@@ -5,43 +5,59 @@ var walk = require('walkdir')
 
 module.exports = function(path, env) {
 	var length = path.length,
-	    config = {};
+	    config = {}, depth, files;
 
 	env = env || process.ENV;
 
 	var relativePathTokens = function(filepath) {
-		return filepath.substr(length + 1).split('/');
+		return filepath.substr(length + 1);
 	}
 
 	var onlyFiles = function(filepath) {
 		return filepath.match(/\..+$/);
 	}
 
-	var grouper = function(tokens) {
-		return tokens.length;
+	var grouper = function(path) {
+		return path.split('/').length;
 	}
 
 	var extract = function(pivot) {
 		return function(file) {
-			var tokens = file[pivot].split('.');
-			config[tokens[0]] = require(path + '/' + file.join('/'));
+			var tokens = file.split('.');
+			var key = file.split('/')[pivot].split('.')[0];
+			config[key] = require(path + '/' + file);
 		}
 	}
 
 	var findEnv = function(files, env) {
 		return files.filter(function(file) {
-			return env === file[0];
+			return startsWith(file, env);
 		});
 	}
 
-	var files = walk.sync(path)
+	var startsWith = function(str, starts) {
+		if (starts === '') return true;
+		if (str == null || starts == null) return false;
+		str = String(str); starts = String(starts);
+		return str.length >= starts.length && str.slice(0, starts.length) === starts;
+	}
+
+	files = walk.sync(path)
 		.filter(onlyFiles)
 		.map(relativePathTokens);
 
-	files = _.groupBy(path, grouper);
+	files = _.groupBy(files, grouper);
 
-	files['1'].forEach(extract(0));
-	findEnv(files['2'], env).forEach(extract(1));
+	depth = Object.keys(files).length;
+	envs = env.split('-');
+
+	if (files[1]) {
+		files[1].forEach(extract(0));
+	}
+
+	for (var i = 1; i < depth; ++i) {
+		findEnv(files[depth], envs.slice(0, i).join('/')).forEach(extract(i));
+	}
 
 	return config;
 }
